230b905827e841f653e5633af3959a062796f2a8
plxsas <sale12302006@yahoo.com>
2020-06-26 08:49:31 +0100

    change run.py

--- run.py
+++ run.py
@@ -8,7 +8,7 @@ app.secret_key = 'some_secret'
 
 @app.route("/")
 def index():
-    return render_template("templates/index.html")
+    return render_template("index.html")
 
 
 
@@ -17,7 +17,7 @@ def about():
     data = []
     with open("static/data/company.json", "r") as json_data:
         data = json.load(json_data)
-    return render_template("templates/about.html", page_title="About", company=data)
+    return render_template("about.html", page_title="About", company=data)
 
 
 
@@ -31,19 +31,19 @@ def about_member(member_name):
             if obj["url"] == member_name:
                 member = obj
 
-    return render_template("templates/member.html", member=member)
+    return render_template("member.html", member=member)
 
 
 @app.route("/contact", methods=["GET", "POST"])
 def contact():
     if request.method == "POST":
         flash("Thanks {}, we have received your message!".format(request.form["name"]))
-    return render_template("templates/contact.html", page_title="Contact")
+    return render_template("contact.html", page_title="Contact")
 
 
 @app.route("/careers")
 def careers():
-    return render_template("templates/careers.html", page_title="Careers")
+    return render_template("careers.html", page_title="Careers")
 
 
 if __name__ == '__main__':
